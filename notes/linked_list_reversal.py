95e2e05750dabe82609b73a6dabc2ea56556b9a4
Olivia Toolson <livytoolson@gmail.com>
2021-01-19 12:08:19 -0700

    node reversal example

--- notes/linked_list_reversal.py
+++ notes/linked_list_reversal.py
@@ -7,6 +7,22 @@
 # We want each node to point to the previous one rather than to the next one
 # Need 3 pointers chasing each other
 
-prev = cur
-cur = next
-next = cur.next
+def reverse(head_of_list):
+    current_node = head_of_list
+    previous_node = None
+    next_node = None
+​
+    # Until we have 'fallen off' the end of the list
+    while current_node:
+        # Copy a pointer to the next element
+        # before we overwrite current_node.next
+        next_node = current_node.next
+​
+        # Reverse the 'next' pointer
+        current_node.next = previous_node
+​
+        # Step forward in the list
+        previous_node = current_node
+        current_node = next_node
+​
+    return previous_node
